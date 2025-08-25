f6ae73bd18a5b2bebdbb261fe1941dc5b21c0591
ashik-ahmed <ashikahmed121@gmail.com>
2025-08-25 22:33:15 +0600

    redux configured and login page created

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -12,13 +12,12 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 import { ModeToggle } from "./ModeToggler"
+import { Link } from "react-router"
 
 // Navigation links array to be used in both desktop and mobile menus
 const navigationLinks = [
-  { href: "#", label: "Home", active: true },
-  { href: "#", label: "Features" },
-  { href: "#", label: "Pricing" },
-  { href: "#", label: "About" },
+  { href: "/", label: "Home", active: true },
+  { href: "/about", label: "About" },
 ]
 
 export default function Navbar() {
@@ -68,11 +67,9 @@ export default function Navbar() {
                   {navigationLinks.map((link, index) => (
                     <NavigationMenuItem key={index} className="w-full">
                       <NavigationMenuLink
-                        href={link.href}
                         className="py-1.5"
-                        active={link.active}
                       >
-                        {link.label}
+                        <Link to={link.href}>{link.label}</Link>
                       </NavigationMenuLink>
                     </NavigationMenuItem>
                   ))}
@@ -82,20 +79,18 @@ export default function Navbar() {
           </Popover>
           {/* Main nav */}
           <div className="flex items-center gap-6">
-            <a href="#" className="text-primary hover:text-primary/90">
+            <Link to="/" className="text-primary hover:text-primary/90">
               <Logo />
-            </a>
+            </Link>
             {/* Navigation menu */}
             <NavigationMenu className="max-md:hidden">
               <NavigationMenuList className="gap-2">
                 {navigationLinks.map((link, index) => (
                   <NavigationMenuItem key={index}>
                     <NavigationMenuLink
-                      active={link.active}
-                      href={link.href}
                       className="text-muted-foreground hover:text-primary py-1.5 font-medium"
                     >
-                      {link.label}
+                      <Link to={link.href}>{link.label}</Link>
                     </NavigationMenuLink>
                   </NavigationMenuItem>
                 ))}
@@ -108,11 +103,8 @@ export default function Navbar() {
           <div>
             <ModeToggle />
           </div>
-          <Button asChild variant="ghost" size="sm" className="text-sm">
-            <a href="#">Sign In</a>
-          </Button>
-          <Button asChild size="sm" className="text-sm">
-            <a href="#">Get Started</a>
+          <Button asChild size="sm" className="text-sm text-white">
+            <Link to="/login">Sign In</Link>
           </Button>
         </div>
       </div>
---
+++ src/lib/axios.ts
@@ -0,0 +1,26 @@
+
+import config from "@/config";
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+    baseURL: config.baseUrl,
+});
+
+axiosInstance.interceptors.request.use(
+    function (config) {
+        console.log("Axios", config);
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
+
+axiosInstance.interceptors.response.use(
+    function onFulfilled(response) {
+        return response;
+    },
+    function onRejected(error) {
+        return Promise.reject(error);
+    }
+);
