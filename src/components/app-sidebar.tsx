9e5fa13c50a30cdb96abc51dee0a1c44054a6c8d
ashik-ahmed <ashikahmed121@gmail.com>
2025-10-20 22:53:57 +0600

    logo and user dropdown added in sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,4 +1,7 @@
 import * as React from "react"
+
+import { SearchForm } from "@/components/search-form"
+import { VersionSwitcher } from "@/components/version-switcher"
 import {
   Sidebar,
   SidebarContent,
@@ -12,13 +15,15 @@ import {
   SidebarRail,
 } from "@/components/ui/sidebar"
 import { Link } from "react-router"
-
+import Logo from "@/assets/icons/Logo"
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-
   return (
     <Sidebar {...props}>
       <SidebarHeader>
+        <div className="px-4 py-2">
+          <Logo />
+        </div>
       </SidebarHeader>
       <SidebarContent>
         {/* We create a SidebarGroup for each parent. */}
--- src/components/layout/UserLayout.tsx
+++ src/components/layout/UserLayout.tsx
@@ -1,10 +1,14 @@
 import type { ReactNode } from "react"
+import { Outlet, useNavigate } from "react-router"
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "../ui/sidebar"
 import { AppSidebar } from "../app-sidebar"
-import { Outlet } from "react-router"
+import { CircleUserRound, Moon, Sun, User } from "lucide-react"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu"
+import { Button } from "../ui/button"
 
 
 const UserLayout = ({ children }: { children: ReactNode }) => {
+    const navigate = useNavigate();
     const sidebarList = {
         navMain: [
             {
@@ -35,15 +39,28 @@ const UserLayout = ({ children }: { children: ReactNode }) => {
         <SidebarProvider>
             <AppSidebar sidebarList={sidebarList} />
             <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                <header className="flex justify-between h-16 shrink-0 items-center gap-2 border-b px-4">
                     <SidebarTrigger className="-ml-1" />
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" size="sm">
+                                <CircleUserRound />
+                                <span className="sr-only">User Account</span>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuItem onClick={() => navigate("/user/profile")}>
+                                Profile
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
                 </header>
                 <div className="flex flex-1 flex-col gap-4 p-4">
                     <Outlet />
                     {/* <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" /> */}
                 </div>
             </SidebarInset>
-        </SidebarProvider>
+        </SidebarProvider >
     )
 }
 
